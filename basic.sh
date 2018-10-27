5b186afd966cbe195dfb6a7e5986ad30a01a20a8
Dishakumra <36642547+Dishakumra@users.noreply.github.com>
2018-10-27 19:06:20 +0530

    find current directoty,current shell
    gives the basic knowledge about shell

--- basic.sh
+++ basic.sh
@@ -2,5 +2,11 @@
 version="20161118" 
 echo "hello, $USER"
 echo $version
+
 date >> basic_date.log
+echo "logged user" $USER
+echo "cuurentshell" $SHELL
+echo "homedirectory" $HOME
+echo "Your os system" $OSTYPE
+echo "current path setting" $PATH
 
